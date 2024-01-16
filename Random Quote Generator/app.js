b7c43092e2dc4d0b78721361d0e1a9b2c719d2fc
Vinay-Bagde-19 <146116197+Vinay-Bagde-19@users.noreply.github.com>
2024-01-16 09:22:29 +0530

    Add files via upload

---
+++ Random Quote Generator/app.js
@@ -0,0 +1,11 @@
+const generateQuote = ()=>{
+    let url="https://type.fit/api/quotes";
+    fetch(url).then(function(response){
+        return response.json()
+    }).then(function(data){
+        let randomNum= Math.floor((Math.random() *10)+1);
+        let randomQuote = data[randomNum];
+        document.getElementById("quote").innerHTML =`${randomQuote.text}`;
+        document.getElementById("author").innerHTML =`${randomQuote.author ? randomQuote.author : ""}`
+    })
+}
